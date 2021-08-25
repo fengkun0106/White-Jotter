9b8fb6c9b6fc5b8fcf3303604931d5217ada8d1c
fengkun666 <fengkun666@126.com>
2021-08-25 16:02:41 +0800

    提交代码

--- wj-vue/src/main.js
+++ wj-vue/src/main.js
@@ -4,15 +4,19 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui' // element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'// element-ui的css
+Vue.use(ElementUI) // 使用elementUI
+
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8843/api'
-
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
--- wj-vue/src/router/index.js
+++ wj-vue/src/router/index.js
@@ -3,10 +3,29 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import AppIndex from '@/home/AppIndex'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
+
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
     {
       path: '/',
       name: 'HelloWorld',
